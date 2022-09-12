789848a7fd8ad2bfd2050947584bff25fa9d1d9e
c0rp3n <ojhitchcock@gmail.com>
2022-09-12 18:58:12 +0100

    Add a TilePlan structure

---
+++ include/cento/centoTilePlan.hpp
@@ -0,0 +1,29 @@
+//
+// Copyright (C) 2022 by Oliver John Hitchcock - github.com/c0rp3n
+//
+
+#ifndef centoTilePlan_hpp
+#define centoTilePlan_hpp
+
+#pragma once
+
+#include "centoNamespace.hpp"
+#include "centoRect.hpp"
+
+#include <compare>
+#include <typeinfo>
+
+CENTO_BEGIN_NAMESPACE
+
+struct TilePlan
+{
+    u64    id;
+    Rect   rect;
+
+    friend std::strong_ordering operator<=>(const TilePlan& lhs, const TilePlan& rhs) = default;
+};
+static_assert(std::is_trivial_v<TilePlan>);
+
+CENTO_END_NAMESPACE
+
+#endif // centoTilePlan_hpp
