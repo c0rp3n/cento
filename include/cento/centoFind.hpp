39d0b6cbd6854a87173d8c58f221933eb936ef71
c0rp3n <ojhitchcock@gmail.com>
2022-09-12 18:58:03 +0100

    Use helper functions in find

--- include/cento/centoFind.hpp
+++ include/cento/centoFind.hpp
@@ -18,16 +18,17 @@ CENTO_FORCEINLINE Tile* findTileAt(const Plane& plane, const Point& point)
 {
     Tile* t = plane.start;
 
-    while (not contains(t->rect, point))
+    while (not contains(rect(t), point))
     {
         // 1. First move up (or down) along the left edges of tiles until a tile
         //    is found whose vertical range contains the desired point.
-        while (t->rect.ur.y < point.y) { t = t->above; }
-        while (t->rect.ll.y > point.y) { t = t->below; }
+        while (upperRight(rect(t)).y < point.y) { t = t->above; }
+        while (lowerLeft(rect(t)).y > point.y) { t = t->below; }
+
         // 2. Then move left (or right) along the bottom edges of tiles until a
         //    tile is found whose horizontal range contains the desired point.
-        while (t->rect.ur.x < point.x) { t = t->right; }
-        while (t->rect.ll.y > point.y) { t = t->left; }
+        while (upperRight(rect(t)).x < point.x) { t = t->right; }
+        while (lowerLeft(rect(t)).y > point.x) { t = t->left; }
 
         // 3. Since the horizontal motion may have caused a vertical
         //    misalignment, steps 1 and 2 may have to be iterated several times
