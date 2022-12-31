432d8c54591f07408d4b3e3daa051ee9176525b0
Oliver John Hitchcock <ojhitchcock@gmail.com>
2022-12-31 16:09:57 +0000

    Remove old C header inclusions

--- acus/main.cpp
+++ acus/main.cpp
@@ -1,6 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//
+// Copyright (C) 2022 by Oliver John Hitchcock - github.com/c0rp3n
+//
+// Distributed under the Boost Software License, Version 1.0.
+//
 
 #include "cento/centoRect.hpp"
 #include "cento/centoCreate.hpp"
