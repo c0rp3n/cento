3286737242736a66a2fbfdfbde260939507875b6
c0rp3n <ojhitchcock@gmail.com>
2025-05-10 14:48:41 +0100

    Add Split Vertical

--- cento/src/split.rs
+++ cento/src/split.rs
@@ -1,8 +1,13 @@
 use crate::*;
 
 pub struct HorzSplit {
-    pub upper: TileKey,
     pub lower: TileKey,
+    pub upper: TileKey,
+}
+
+pub struct VertSplit {
+    pub left: TileKey,
+    pub right: TileKey,
 }
 
 impl Plane {
@@ -27,6 +32,7 @@ impl Plane {
 
         // update bounds
         lower.bounds.set_max(Point::new(lower.max_x(), y));
+
         // update stitches
         upper.stitches.below = Some(lkey);
         upper.stitches.above = lower.stitches.above;
@@ -45,6 +51,7 @@ impl Plane {
             other.stitches.below = Some(ukey);
             other_key = other.stitches.left;
         }
+        lower.stitches.above = Some(ukey);
 
         // adjust corner stitches along right edge
         other_key = lower.right();
@@ -57,6 +64,7 @@ impl Plane {
             other.stitches.left = Some(ukey);
             other_key = other.stitches.below;
         }
+        lower.stitches.right = other_key;
 
         // adjust corner stitches along left edge
         other_key = lower.left();
@@ -84,8 +92,94 @@ impl Plane {
         unsafe { *slots.get_unchecked_mut(lkey) = lower };
 
         Some(HorzSplit {
-            upper: ukey,
             lower: lkey,
+            upper: ukey,
+        })
+    }
+
+    pub fn split_vert(&mut self, key: TileKey, x: i32) -> Option<VertSplit> {
+        let slots = self.slots_mut();
+
+        let lkey = key;
+        let mut left: Tile = unsafe { *slots.get_unchecked(lkey) };
+
+        if left.min_x() >= x || left.max_x() <= x {
+            return None;
+        }
+
+        let rkey = slots.insert(Tile::new(
+            Rect::new(
+                Point::new(left.min_x(), left.min_y()),
+                Point::new(x, left.max_y()),
+            ),
+            None,
+        ));
+        let mut right: Tile = unsafe { *slots.get_unchecked(rkey) };
+
+        // update bounds
+        left.bounds.set_max(Point::new(x, left.max_y()));
+
+        // update stitches
+        right.stitches.left = Some(lkey);
+        right.stitches.right = left.stitches.right;
+        right.stitches.above = left.stitches.above;
+
+        let mut other_key;
+
+        // adjust corner stitches along the right edge
+        other_key = left.right();
+        while other_key.is_some() {
+            let other = unsafe { slots.get_unchecked_mut(other_key?) };
+            if other.stitches.below != Some(lkey) {
+                break;
+            }
+
+            other.stitches.left = Some(rkey);
+            other_key = other.stitches.below;
+        }
+        left.stitches.right = Some(rkey);
+
+        // adjust corner stitches along the top edge
+        other_key = left.above();
+        while other_key.is_some() {
+            let other = unsafe { slots.get_unchecked_mut(other_key?) };
+            if other.min_x() < x {
+                break;
+            }
+
+            other.stitches.below = Some(rkey);
+            other_key = other.stitches.left;
+        }
+        left.stitches.above = other_key;
+
+        // adjust corner stitches along the bottom edge
+        other_key = left.below();
+        while other_key.is_some() {
+            let other = unsafe { slots.get_unchecked_mut(other_key?) };
+            if other.min_x() > x {
+                break;
+            }
+            other_key = other.stitches.right;
+        }
+
+        right.stitches.below = other_key;
+        while other_key.is_some() {
+            let other = unsafe { slots.get_unchecked_mut(other_key?) };
+            if other.above() != Some(lkey) {
+                break;
+            }
+
+            other.stitches.above = Some(rkey);
+            other_key = other.stitches.right;
+        }
+
+        // update the data in the slot map...
+        unsafe { *slots.get_unchecked_mut(lkey) = left };
+        unsafe { *slots.get_unchecked_mut(rkey) = right };
+
+        Some(VertSplit {
+            left: lkey,
+            right: rkey,
         })
     }
 }
